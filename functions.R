4254d9583c780a648a357384eeca451478ac54e6
Kaley Regner <66963723+kalesreg@users.noreply.github.com>
2021-06-29 19:53:30 -0400

    Update functions.R
    Couple small changes and added balance and bottom line calculation functions

--- functions.R
+++ functions.R
@@ -2,9 +2,6 @@
 ## organizations into relevant categories, save the tidy data, and create
 ## summary data.
 
-## Would like to find a way to rework the notes/special instruction section of
-## new.institution, read.transaction, and read.transaction.folder.
-
 library(data.table)
 library(stringr)
 library(lubridate)
@@ -56,15 +53,20 @@ new.institution <- function(institution, file_name, type = "credit", skip = 0, d
 ## read into the environment. If it hasn't, it reads it in or creates the file
 ## and reads it in. (Complete)
 
-## Make changes to functions involving transaction data. Cascades through many.
-
 transaction.data <- function() {
         if (month(Sys.Date()) == 1) {
+                year <- year(Sys.Date())-1
+                month <- 11
+        } else if (month(Sys.Date()) == 2) {
                 year <- year(Sys.Date())-1
                 month <- 12
         } else {
                 year <- year(Sys.Date())
-                month <- month(Sys.Date())-1
+                if (month(Sys.Date()) == 12) {
+                        month <- month(Sys.Date())-2
+                } else {
+                        month <- paste("0", month(Sys.Date())-2, sep = "")
+                }
         }
         file <- paste("./tidy-data/", year, "-", month, ".csv", sep = "")
         if (any(str_detect(ls(envir=.GlobalEnv), "transactions")) == FALSE) {
@@ -208,8 +210,6 @@ category.sort <- function(data) {
 
 ## review.category is used to check the descriptions under a given category 
 ## belong there. Corresponds to Step 6 in the walkthrough. (Complete)
-
-## Would like to streamline other.desc, add.desc, and review.category.
 
 review.category <- function(data, category = "all") {
         category.data()
@@ -403,4 +403,271 @@ year.sum <- function(data, years) {
         years_list <- lapply(years, function(x) each.sum(data, x))
         comb <- Reduce(function(...) merge(..., all = TRUE), years_list)
         return(comb)
+}
+                       
+## balance.data function checks to see if the balance data set has been read
+## into the environment. If it hasn't, it either reads in data from 2 months ago
+## or creates and reads in the file. (Complete)
+
+balance.data <- function() {
+        if (month(Sys.Date()) == 1) {
+                year <- year(Sys.Date())-1
+                month <- 11
+        } else if (month(Sys.Date()) == 2) {
+                year <- year(Sys.Date())-1
+                month <- 12
+        } else {
+                year <- year(Sys.Date())
+                if (month(Sys.Date()) == 12) {
+                        month <- month(Sys.Date())-2
+                } else {
+                        month <- paste("0", month(Sys.Date())-2, sep = "")
+                }
+        }
+        file <- paste("./bottom-line/balances-", year, "-", month, ".csv", sep = "")
+        if (any(str_detect(ls(envir=.GlobalEnv), "balances")) == FALSE) {
+                if (any(str_detect(list.files("./bottom-line", full.names = TRUE), file)) == TRUE) {
+                        balances <<- fread(file) 
+                } else {
+                        balances <<- setNames(data.table(matrix(nrow = 0, ncol = 6)), c("Institution", "Type", "Year", "Month", "Start_Balance", "End_Balance"))
+                        balances <<- balances[, lapply(balances[, 3:6], as.numeric), lapply(balances[, 1:2], as.character)]
+                        setkey(balances, "Institution", "Type", "Year", "Month")
+                        fwrite(balances, file)
+                }
+        }
+}
+
+## bottom.line.data is used to check to see if the bottom_line data has been 
+## read into the environment. If it hasn't, it reads it in or creates the file
+## and reads it in. (Complete)
+
+bottom.line.data <- function() {
+        if (month(Sys.Date()) == 1) {
+                year <- year(Sys.Date())-1
+                month <- 11
+        } else if (month(Sys.Date()) == 2) {
+                year <- year(Sys.Date())-1
+                month <- 12
+        } else {
+                year <- year(Sys.Date())
+                if (month(Sys.Date()) == 12) {
+                        month <- month(Sys.Date())-2
+                } else {
+                        month <- paste("0", month(Sys.Date())-2, sep = "")
+                }
+        }
+        file <- paste("./bottom-line/bottom-line-", year, "-", month, ".csv", sep = "")
+        if (any(str_detect(ls(envir=.GlobalEnv), "bottom_line")) == FALSE) {
+                if (any(str_detect(list.files("./bottom-line", full.names = TRUE), file)) == TRUE) {
+                        bottom_line <<- fread(file) 
+                } else {
+                        bottom_line <<- setNames(data.table(matrix(nrow = 0, ncol = 5)), c("Year", "Month", "Start", "End", "Change"))
+                        bottom_line <<- bottom_line[, lapply(bottom_line, as.numeric)]
+                        setkey(bottom_line, "Year", "Month")
+                        fwrite(bottom_line, file)
+                }
+        }
+}
+
+## add.balance adds the balance start and end for each institution, for each
+## month of the year. Checks if previous month's end_balance matches inputted 
+## months start_balance. Checks if next month's start_balance matches inputted 
+## months end_balance. Adds row if needed. (Complete)
+
+add.balance <- function(institution, type = "Credit", year, month, start_balance, end_balance) {
+        balance.data()
+        previous.month <- function(institution, type, year, month, start_balance) {
+                if (month == 1) {
+                        previous_month <- 12
+                        previous_year <- year-1
+                } else {
+                        previous_month <- month-1
+                        previous_year <- year
+                }
+                previous_row <- data.table(Institution = toupper(institution), Type = toupper(type), Year = as.integer(previous_year), Month = as.integer(previous_month), Start_Balance = NA, End_Balance = start_balance)
+                for (i in 1:nrow(balances)) {
+                        if (all(balances[i, 1:4] == previous_row[, 1:4])) {
+                                if (is.na(balances[i, "End_Balance"]) == TRUE) {
+                                        balances[i, "End_Balance"] <- start_balance
+                                        print("Previous Month's End Balance added.")
+                                } else {
+                                        if (start_balance != balances[i, "End_Balance"]) {
+                                                message <- paste("Previous Month's End balance updated from", balances[i, "End_Balance"])
+                                                balances[i, "End_Balance"] <- start_balance
+                                                print(message)
+                                        } else {
+                                                print("Previous Month's End Balance correct.")
+                                        }
+                                }
+                                break
+                        }
+                        if (i == nrow(balances)) {
+                                balances <- rbind(balances, previous_row)
+                                setkey(balances, "Institution", "Type", "Year", "Month")
+                                print("Added previous month's end balance")
+                        }
+                }
+                return(balances)
+        }
+        next.month <- function(institution, type, year, month, end_balance) {
+                if (month == 12) {
+                        next_month <- 1
+                        next_year <- year+1
+                } else {
+                        next_month <- month+1
+                        next_year <- year
+                }
+                next_row <- data.table(Institution = toupper(institution), Type = toupper(type), Year = as.integer(next_year), Month = as.integer(next_month), Start_Balance = end_balance, End_Balance = NA)
+                for (k in 1:nrow(balances)) {
+                        if (all(balances[k, 1:4] == next_row[, 1:4])) {
+                                if (is.na(balances[k, "Start_Balance"]) == TRUE) {
+                                        balances[k, "Start_Balance"] <- end_balance
+                                        print("Next Month's Start Balance added.")
+                                } else {
+                                        if (end_balance != balances[k, "Start_Balance"]) {
+                                                message <- paste("Next Month's Start balance updated from", balances[k, "Start_Balance"])
+                                                balances[k, "Start_Balance"] <- end_balance
+                                                print(message)
+                                        } else {
+                                                print("Next Month's start balance correct.")
+                                        }
+                                }
+                                break
+                        }
+                        if (k == nrow(balances)) {
+                                balances <- rbind(balances, next_row)
+                                setkey(balances, "Institution", "Type", "Year", "Month")
+                                print("Added next month's start balance")
+                        }
+                }
+                return(balances)
+        }
+        new_row <- data.table(Institution = toupper(institution), Type = toupper(type), Year = as.integer(year), Month = as.integer(month), Start_Balance = start_balance, End_Balance = end_balance)
+        if (any(sapply(1:nrow(balances), function(x) all(balances[x, 1:2] == new_row[, 1:2]))) == FALSE) {
+                if (askYesNo("Do you want to add a new institution and type to the balance sheet? ") == FALSE) {
+                        stop("Not added to balance sheet due to invalid institution and type.")
+                }
+        }
+        for (j in 1:nrow(balances)) {
+                if (all(balances[j, 1:4] == new_row[, 1:4])) {
+                        if (is.na(balances[j, "Start_Balance"]) == TRUE) {
+                                balances[j, "Start_Balance"] <<- start_balance
+                                print("Start Balance updated.")
+                                balances <<- previous.month(institution, type, year, month, start_balance)
+                        } else {
+                                if (start_balance != balances[j, "Start_Balance"]) {
+                                        message <- paste("Start balance updated from", balances[j, "Start_Balance"])
+                                        balances[j, "Start_Balance"] <<- start_balance
+                                        print(message)
+                                        balances <<- previous.month(institution, type, year, month, start_balance)
+                                } else {
+                                        print("Previous start balance correct.")
+                                }
+                        }
+                        if (is.na(balances[j, "End_Balance"]) == TRUE) {
+                                balances[j, "End_Balance"] <<- end_balance
+                                print("End Balance added.")
+                                balances <<- next.month(institution, type, year, month, end_balance)
+                        } else {
+                                if (end_balance != balances[j, "End_Balance"]) {
+                                        message <- paste("End balance updated from", balances[j, "End_Balance"])
+                                        balances[j, "End_Balance"] <<- end_balance
+                                        print(message)
+                                        balances <<- next.month(institution, type, year, month, end_balance)
+                                } else {
+                                        print("Previous end balance correct.")
+                                }
+                        }
+                        break
+                } 
+                if (j == nrow(balances)) {
+                        balances <<- rbind(balances, new_row)
+                        setkey(balances, "Institution", "Type", "Year", "Month")
+                        print("Month balances added.")
+                        balances <<- previous.month(institution, type, year, month, start_balance)
+                        balances <<- next.month(institution, type, year, month, end_balance)
+                }
+        }
+        if (month(Sys.Date()) == 1) {
+                sys_year <- year(Sys.Date())-1
+                sys_month <- 12
+        } else {
+                sys_year <- year(Sys.Date())
+                if (month(Sys.Date()) > 10) {
+                        sys_month <- month(Sys.Date())-1
+                } else {
+                        sys_month <- paste("0", month(Sys.Date())-1, sep = "")
+                }
+        }
+        file <- paste("./bottom-line/balances-", sys_year, "-", sys_month, ".csv", sep = "")
+        fwrite(balances, file)
+}
+
+## month.bottom.line used to calculate the bottom line for a specified month. (Complete)
+
+month.bottom.line <- function (year, month) {
+        balance.data()
+        bottom.line.data()
+        month_data <- balances[Year == year & Month == month]
+        month_data[Type == "CREDIT", Start_Balance := -Start_Balance]
+        month_data[Type == "CREDIT", End_Balance := -End_Balance]
+        start <- sum(month_data[, "Start_Balance"])
+        end <- sum(month_data[, "End_Balance"])
+        change <- end-start
+        for (i in 1:nrow(bottom_line)) {
+                if (bottom_line[i, "Year"] == year && bottom_line[i, "Month"] == month) {
+                        if (bottom_line[i, "Start"] == start && bottom_line[i, "End"] == end && bottom_line[i, "Change"] == change) {
+                                print("Bottom line previously calculated.")
+                        } else {
+                                bottom_line[i, "Start"] <<- start 
+                                bottom_line[i, "End"] <<- end 
+                                bottom_line[i, "Change"] <<- change
+                                print("Bottom line data updated.")
+                        }
+                        print(bottom_line[i])
+                        break
+                } 
+                if (i == nrow(bottom_line)) {
+                        new_row <- data.table(Year = year, Month = month, Start= start, End = end, Change = change)
+                        bottom_line <<- rbind(bottom_line, new_row)
+                        print("New bottom line added.")
+                        setkey(bottom_line, "Year", "Month")
+                        print(new_row)
+                }
+        }
+        if (month(Sys.Date()) == 1) {
+                sys_year <- year(Sys.Date())-1
+                sys_month <- 12
+        } else {
+                sys_year <- year(Sys.Date())
+                if (month(Sys.Date()) > 10) {
+                        sys_month <- month(Sys.Date())-1
+                } else {
+                        sys_month <- paste("0", month(Sys.Date())-1, sep = "")
+                }
+        }
+        file <- paste("./bottom-line/bottom-line-", sys_year, "-", sys_month, ".csv", sep = "")
+        fwrite(bottom_line, file)
+}
+
+## bottom.line.check makes sure bottom line change matches change represented by transactions. (Complete)
+
+bottom.line.check <- function(trans_data, year, month) {
+        bottom.line.data()
+        summary <- month.sum(trans_data, year, month)
+        trans_data <- sum(summary[, 2])
+        month.bottom.line(year, month)
+        rel_row <- data.table(Year = year, Month = month)
+        for (i in 1:nrow(bottom_line)) {
+                if (all(bottom_line[i, 1:2] == rel_row)) {
+                        rel_bottom_line <- bottom_line[i]
+                }
+        }
+        check <- trans_data-rel_bottom_line[, "Change"]
+        if (abs(check) <= 0.01) {
+                print("Transactions and bottom line match.")
+        } else {
+                print("Transactions and bottom line don't match.")
+                return(check)
+        }
 }
